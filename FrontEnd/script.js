7edade6369df8e1d07f9e344cbfa00a72a1de4fa
Mnovis <novisdev@hotmail.com>
2026-03-26 14:29:50 -0300

    Quantidade Formatação de Valores e Visual extra para lucro

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -7,17 +7,30 @@ form.addEventListener("submit", (e) => {
   const name = document.getElementById("ProductName").value;
   const purchase = Number(document.getElementById("PurchasePrice").value);
   const selling = Number(document.getElementById("SellingPrice").value);
-  const profit = selling - purchase;
-  const profitColor = profit >=0 ? "green" : "red";
+  const quantity = Number(document.getElementById("Quantity").value);
+
+  const profit = calculateProfit(purchase, selling, quantity);
+  const profitColor = profit >= 0 ? "green" : "red";
 
   const item = document.createElement("li");
   item.innerHTML = `
-  ${name} | Compra:${purchase} | Venda:${selling} | Lucro:${profit}
+  ${name} | Quantidade: ${quantity} | Valor Unitário: ${formatCurrency(purchase)} | Valor de Venda Unitária: ${formatCurrency(selling)} | 
+  <span style="color:${profitColor}"> Lucro: ${formatCurrency(profit)}</span>
   <button onclick="this.parentElement.remove()">Excluir</button>`;
 
   list.appendChild(item);
   form.reset();
 });
 
+// Funções
 
+function calculateProfit(purchase, selling, quantity) {
+  return (selling - purchase) * quantity;
+}
 
+function formatCurrency(value) {
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(value);
+}
